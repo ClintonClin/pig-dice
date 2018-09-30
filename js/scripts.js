c26eb129d864700abc324259f352f75d69370787
ClintonClin <clintonclin00@gmail.com>
2018-10-01 01:19:14 +0300

    added javascript file that randomizes numbers as a dice. Also added buttons to the index page

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function rollDice(){
+   var randomNumber;
+   var rollDice;
+   var score;
+
+    var score=+randomNumber;
+
+      randomNumber=document.getElementById('dice1').innerHTML = Math.floor((Math.random()*6)+ 1);
+      document.getElementById('score1').innerHTML ="Score: "+randomNumber;
+
+
+
+
+
+}
