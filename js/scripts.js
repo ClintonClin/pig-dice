a054601a37569eb167fb1d25c6b65dd69cb199ac
ClintonClin <clintonclin00@gmail.com>
2018-10-01 09:42:42 +0300

    wrote code to perform tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 function rollDice(){
    var randomNumber;
    var rollDice;
-   var score;
-
-    var score=+randomNumber;
-
+   var score=0;
+   var score1;
+   var score2;
+      //score1=+randomNumber;
+      //score2=+randomNumber;
       randomNumber=document.getElementById('dice1').innerHTML = Math.floor((Math.random()*6)+ 1);
-      document.getElementById('score1').innerHTML ="Score: "+randomNumber;
+      //document.getElementById('score1').innerHTML ="Score: "+randomNumber;
 
-
-
-
-
+function hold(){
+    score1 = randomNumber+score;
+    document.getElementById('score1').innerHTML ="Score: "+score1;
+}
 }
